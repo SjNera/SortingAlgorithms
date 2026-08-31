606ad0813938ea758cc2a36a29f1882f5a872b4b
Sjnera <nerastephenjarcelle@gmail.com>
2026-08-31 23:02:32 +0800

    Quicksort and Merge sort Implemented. No step by step process yet.

--- AlgorithmExamples.java
+++ AlgorithmExamples.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class DataType<T> {
         private T[] array;
 
@@ -9,18 +11,22 @@ class DataType<T> {
             return array;
         }
 
-        public void set(T[] array) {
-            this.array = array;
-        }
-
-
-
 }
 
 
 public class AlgorithmExamples {
     public static void main(String[] args) {
-    // Call the generic class here    
+    // Call the generic class here
+    // Example usage of the DataType class
+        Integer[] intArray = {5, 2, 9, 1, 5, 6};
+        DataType<Integer> intData = new DataType<>(intArray);
+        System.out.println("Integer Array: ");
+        printArray(intData.get());
+
+        String[] strArray = {"apple", "orange", "banana", "grape"};
+        DataType<String> strData = new DataType<>(strArray);
+        System.out.println("String Array: ");
+        printArray(strData.get());    
     }
     
 
@@ -32,21 +38,132 @@ public class AlgorithmExamples {
 
     }
 
-    public static <T> void mergeSort(T[] array) {
+    /**
+     * Merge two subarrays of array[].
+     * First subarray is array[left..middle]
+     * Second subarray is array[middle+1..right]
+    */ 
+    public static <T extends Comparable <T>> void merge(T[] array, int left,int middle, int right ) {
+        int node1 = middle - left + 1;
+        int node2 = right - middle;
 
+        // Create temporary arrays
+        T[] leftArray = (T[]) new Object[node1];
+        T[] rightArray = (T[]) new Object[node2];
+
+        for (int i = 0; i < node1; ++i) {
+            leftArray[i] = array[left + i];
+            System.out.println("Merging: " + leftArray[i]);
+        }
+        for (int j = 0; j < node2; ++j) {
+            rightArray[j] = array[middle + 1 + j];
+            System.out.println("Merging: " + rightArray[j]);
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < node1 && j < node2) {
+            if (leftArray[i].compareTo(rightArray[j]) <= 0) {
+                array[k] = leftArray[i];
+                i++;
+
+                System.out.println("Merging: " + array[k]);
+            } else {
+                array[k] = rightArray[j];
+                j++;
+                System.out.println("Merging: " + array[k]);
+            }}
+
+        while (i < node1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+            System.out.println("Merging: " + array[k]);
+        }
+
+        while (j < node2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+            System.out.println("Merging: " + array[k]);
+        }
+        }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            // Recursively sort first and second halves
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+
+            // Merge the sorted halves
+            merge(array, left, middle, right);
+        }
     }
 
-    public static <T> void quickSort(T[] array) {
 
+    /**
+     * Random QuickSort algorithm implementation for generic types.
+     * For this, the type T must implement the Comparable interface to allow comparison between elements.
+     * 
+     * @param <T>
+     * @param array
+     * 
+     */
+
+    // This function is for calculating random pivot index for quicksort 
+
+    private static <T> void randomPivot(T[] array, int left, int right) {
+        Random rand = new Random();
+        int pivotIndex = left + rand.nextInt(right - left + 1);
+        
+        T temp1 = array[pivotIndex];
+        array[pivotIndex] = array[right];
+        array[right] = temp1;        
+    } 
+
+    private static <T extends Comparable<T>> int partition(T[] array, int left, int right) {
+        randomPivot(array,left,right);  
+        int pivotIndex = right;
+
+        int i = (left - 1);
+
+        for (int j = left; j < right; j++) {
+            if (array[j].compareTo(array[pivotIndex]) <= 0){
+                i++;
+
+                T temp2 = array[i];
+                array[i] = array[j];
+                array[j] = temp2;
+
+            }  
+        }
+
+        T temp = array[i + 1];
+        array[i + 1] = array[pivotIndex];
+        array[pivotIndex] = temp;
+
+
+        return i + 1;
+        
+    }
+ 
+    public static <T extends Comparable<T>> void quickSort(T[] array) {
+        
     }
 
+    
 
     // Helper methods for sorting algorithms can be added here
-    public void printArray(T[] array) {
+    public static <T> void printArray(T[] array) {
         for (T element : array) {
             System.out.print(element + " ");
         }
         System.out.println();
     }
+
+
 
 }
